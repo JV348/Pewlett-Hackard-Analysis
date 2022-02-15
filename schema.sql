4c293910d328fc51cbddd453faca1c31a99307f4
vil141 <j.villanueva5@utexas.edu>
2022-02-14 22:35:48 -0600

    Updating files and script

--- schema.sql
+++ schema.sql
@@ -52,10 +52,13 @@ CREATE TABLE titles (
 DROP TABLE titles CASCADE;
 
 CREATE TABLE dept_emp (
-  dept_no VARCHAR(10) NOT NULL,
-  count_of_dept_no INT NOT NULL,
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (dept_no)
+  PRIMARY KEY (emp_no, dept_no)
 );
 
 DROP TABLE dept_emp CASCADE;
